23bb5fd8e799fdf23b80ad1d53c2691ef93ee5da
elhazen98 <elhazen98@gmail.com>
2025-02-03 15:46:45 +0700

    add placeholder to date time input

--- src/app/_components/_components/input-date2.jsx
+++ src/app/_components/_components/input-date2.jsx
@@ -1,11 +1,9 @@
-import { now, getLocalTimeZone } from "@internationalized/date";
-
 export const InputDate2 = ({ name, title }) => {
     return (
         <div className="max-w-xl gap-4 text-xs bg-slate-200 rounded-xl p-3">
             <div className="font-bold">{title}</div>
             <input
-                defaultValue={now(getLocalTimeZone())}
+                placeholder="enter date and time"
                 required
                 type="datetime-local"
                 name={name}
