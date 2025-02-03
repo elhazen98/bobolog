9f3f5dbee949e55ee08fb20fb059a6d0ff6860a6
elhazen98 <elhazen98@gmail.com>
2025-02-03 15:39:07 +0700

    add default value to date input

--- src/app/_components/_components/input-date2.jsx
+++ src/app/_components/_components/input-date2.jsx
@@ -1,8 +1,11 @@
+import { now, getLocalTimeZone } from "@internationalized/date";
+
 export const InputDate2 = ({ name, title }) => {
     return (
         <div className="max-w-xl gap-4 text-xs bg-slate-200 rounded-xl p-3">
             <div className="font-bold">{title}</div>
             <input
+                defaultValue={now(getLocalTimeZone())}
                 required
                 type="datetime-local"
                 name={name}
